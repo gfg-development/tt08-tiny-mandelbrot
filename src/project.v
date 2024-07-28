6ce33bca32750323264c175e3836eb58a5130d43
Gerrit Grutzeck <g.grutzeck@gfg-development.de>
2024-07-28 22:17:47 +0200

    Fixed multiple drivers

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_gfg_development_tinymandelbrot (
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out        = 0;
   assign uio_oe         = 0;
-  assign uo_out[6 : 5]  = 0;
+  assign uo_out[6]      = 0;
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
   wire reset;
