324248123dc71a60bc79b9441b845defe7118b05
Gerrit Grutzeck <g.grutzeck@gfg-development.de>
2024-07-29 14:52:05 +0200

    Added golden image for comparision to the RP2040 mode test

--- test/test.py
+++ test/test.py
@@ -13,6 +13,17 @@ CR_OFFSET = - (511 * WIDTH // 640) * SCALING
 CI_OFFSET = - (HEIGHT // 2) * SCALING
 MAX_CTR = 15
 
+def read_ppm(fname):
+    with open(fname) as f:
+        # Ignore the header
+        for _ in range(3):
+            f.readline()
+        pixel_values = []
+
+        for line in f:
+            pixel_values.extend([int(x) for x in line.split()])
+    return pixel_values
+
 async def configure(dut):
     # Load configuration via shift register
     configuration = (MAX_CTR << 26) | ((SCALING - 1) << 22) | ((CI_OFFSET & 0x7FF) << 11) | (CR_OFFSET & 0x7FF)
@@ -26,7 +37,7 @@ async def configure(dut):
     dut.ui_in[4].value = 0
     await ClockCycles(dut.clk, 10)
 
-#@cocotb.test()
+@cocotb.test()
 async def test_rp2040_mode(dut):
     dut._log.info("Start")
 
@@ -56,19 +67,27 @@ async def test_rp2040_mode(dut):
 
     await FallingEdge(dut.user_project.finished)
 
+    golden_image = read_ppm("image_15_4_-1020_-480.golden.ppm")
+    image = []
+
     with open("image_{}_{}_{}_{}.ppm".format(MAX_CTR, SCALING, CR_OFFSET, CI_OFFSET), "w+") as f:
         f.write("P2\r\n{} {}\r\n15\r\n".format(WIDTH, HEIGHT))
         for y in range(HEIGHT):
-            print("Line: {}".format(y))
+            dut._log.info("Line: {}".format(y))
             for _ in range(WIDTH):
                 await FallingEdge(dut.user_project.running)
                 await RisingEdge(dut.clk)
-                f.write("{} ".format(int(str(dut.uo_out.value[4 : 7]), 2)))
+                pixel_value = int(str(dut.uo_out.value[4 : 7]), 2)
+                image.append(pixel_value)
+                f.write("{} ".format(pixel_value))
             f.write("\r\n")
 
     # Finish flag is set
     assert dut.uo_out[5].value == 1
 
+    # Ensure that the image is the golden one
+    for (x, y) in zip(golden_image, image):
+        assert x == y
 
 @cocotb.test()
 async def test_vga_mode(dut):
@@ -97,5 +116,8 @@ async def test_vga_mode(dut):
     dut.ui_in[0].value = 1
     await ClockCycles(dut.clk, 1)
     dut.ui_in[0].value = 0
+
+    # Wait for image to be generated
+    await RisingEdge(dut.user_project.finished)
 
     await ClockCycles(dut.clk, 1024*1024)
