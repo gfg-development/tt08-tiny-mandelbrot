54e9ba533003b8e380651bfeaf3cde9392f8643f
Gerrit Fabian Grutzeck <ggrutzeck@mpifr-bonn.mpg.de>
2024-08-21 12:44:02 +0200

    Added VGA image test and rewrote VGA driver

--- test/tb.v
+++ test/tb.v
@@ -23,17 +23,39 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  wire       running;
-  wire       finished;
+  wire          running;
+  wire          finished;
 
-  wire       vsync;
-  wire       hsync;
+  wire          vsync;
+  wire          hsync;
+  wire [1 : 0]  red;
+  wire [1 : 0]  green;
+  wire [1 : 0]  blue;
+
+  wire          read;
+  wire          reset_read_ptr;
+
+  wire          write;
+  wire          reset_write_ptr;
+  wire [3 : 0]  write_data;
 
   assign running  = uo_out[4];
   assign finished = uo_out[5];
 
   assign vsync = uo_out[3];
   assign hsync = uo_out[7];
+
+  assign red   = {uo_out[0], uo_out[4]};
+  assign green = {uo_out[1], uo_out[5]};
+  assign blue  = {uo_out[2], uo_out[6]};
+
+  assign read             = uio_out[7];
+  assign reset_read_ptr   = uio_out[6];
+
+  assign write            = uio_out[5];
+  assign reset_write_ptr  = uio_out[4];
+
+  assign write_data       = uio_out[3 : 0];
 
   // Replace tt_um_example with your module name:
   tt_um_gfg_development_tinymandelbrot user_project (
--- test/test.py
+++ test/test.py
@@ -99,12 +99,14 @@ async def test_rp2040_mode(dut):
 VSYNC_MASK = 0x08
 HSYNC_MASK = 0x80
 
+VGA_WIDTH               =  800
 LINE_VISIBLE_AREA       =  800 * 25
 LINE_FRONT_PORCH        =   40 * 25
 LINE_SYNC_PULSE         =  128 * 25
 LINE_BACK_PORCH         =   88 * 25
 WHOLE_LINE              = 1056 * 25
 
+VGA_HEIGHT              = 600
 FRAME_VISIBLE_AREA      = 600 * WHOLE_LINE
 FRAME_FRONT_PORCH       =   1 * WHOLE_LINE
 FRAME_SYNC_PULSE        =   4 * WHOLE_LINE
@@ -150,67 +152,121 @@ async def test_mode_vga(dut):
     pixel_nr = 0
     image = []
 
-    with open("image_vga_{}_{}_{}_{}.ppm".format(MAX_CTR, SCALING, CR_OFFSET, CI_OFFSET), "w+") as f:
-        f.write("P3\r\n{} {}\r\n3\r\n".format(WIDTH, HEIGHT))                
+    framebuffer = [0] * (WIDTH * HEIGHT + 1)
+    read_ptr_h = 0
+    read_ptr_v = 0
+    write_ptr = 0
 
-        for _ in range(2 * 1056 * 628):
-            time = get_sim_time("ns")
-            if dut.vsync.value == 1 and old_vsync == 0:
-                if last_vsync_start != None:
-                    assert time - last_vsync_start == WHOLE_FRAME
-                    assert pixel_nr == 800 * 600
+    old_write = int(dut.write.value)
+    old_read = int(dut.read.value)
+    old_reset_write_ptr = int(dut.reset_write_ptr.value)
+    old_reset_read_ptr = int(dut.reset_read_ptr.value)
 
-                last_vsync_start = time
-                dut.log.info("VSync - start of frame {}".format(frame_nr))
-                frame_nr += 1
+    shift_framebuffer = [0, 0]                
 
-                pixel_nr = 0
+    for _ in range(15 * 1056 * 628):
+        time = get_sim_time("ns")
+        if dut.vsync.value == 1 and old_vsync == 0:
+            if last_vsync_start != None:
+                assert time - last_vsync_start == WHOLE_FRAME
+                assert pixel_nr == 800 * 600
 
-            if dut.vsync.value == 0 and old_vsync == 1:
-                dut.log.info("VSync end")
-                last_vsync_end = time
-                assert last_vsync_end - last_vsync_start == FRAME_SYNC_PULSE
+            last_vsync_start = time
+            
+            if frame_nr != 0:
+                f.close()
 
-            if dut.hsync.value == 1 and old_hsync == 0:
-                dut.log.info("HSync start")
-                if last_hsync_start != None:
-                    assert time - last_hsync_start == WHOLE_LINE
+            frame_nr += 1
+            
+            f = open("image_vga_{}_{}_{}_{}_{}.ppm".format(frame_nr, MAX_CTR, SCALING, CR_OFFSET, CI_OFFSET), "w+")
+            f.write("P3\r\n{} {}\r\n3\r\n".format(VGA_WIDTH, VGA_HEIGHT))
 
-                if last_vsync_start != None:
-                    assert ((time - last_vsync_start) % WHOLE_LINE) == LINE_VISIBLE_AREA + LINE_FRONT_PORCH
+            dut.log.info("VSync - start of frame {}".format(frame_nr))
 
-                last_hsync_start = time
+            pixel_nr = 0
 
+        if dut.vsync.value == 0 and old_vsync == 1:
+            dut.log.info("VSync end")
+            last_vsync_end = time
+            assert last_vsync_end - last_vsync_start == FRAME_SYNC_PULSE
+
+        if dut.hsync.value == 1 and old_hsync == 0:
+            dut.log.info("HSync start")
+            if last_hsync_start is not None:
+                assert time - last_hsync_start == WHOLE_LINE
+
+            if last_vsync_start is not None:
+                assert ((time - last_vsync_start) % WHOLE_LINE) == LINE_FRONT_PORCH
+
+            last_hsync_start = time
+
+            if frame_nr != 0:
                 f.write("\r\n")
 
-            if dut.hsync.value == 0 and old_hsync == 1:
-                dut.log.info("HSync end")
-                last_hsync_end = time
+        if dut.hsync.value == 0 and old_hsync == 1:
+            dut.log.info("HSync end")
+            dut.log.info("Read pointers: {} {}".format(read_ptr_h, read_ptr_v))
+            last_hsync_end = time
+            if last_hsync_start is not None:
                 assert last_hsync_end - last_hsync_start == LINE_SYNC_PULSE
 
-            if last_vsync_end is not None and last_hsync_end is not None:
-                visible_area = (
-                    ((time - last_hsync_end) >= LINE_BACK_PORCH) and
-                    ((time - last_hsync_end) < LINE_VISIBLE_AREA + LINE_BACK_PORCH) and
-                    ((time - last_vsync_end) >= FRAME_BACK_PORCH) and
-                    ((time - last_vsync_end) < FRAME_VISIBLE_AREA + FRAME_BACK_PORCH)
-                )
-            else:
-                visible_area = False
+        if last_vsync_end is not None and last_hsync_end is not None:
+            visible_area = (
+                ((time - last_hsync_end) >= LINE_BACK_PORCH) and
+                ((time - last_hsync_end) < (LINE_VISIBLE_AREA + LINE_BACK_PORCH)) and
+                ((time - last_vsync_end) >= FRAME_BACK_PORCH) and
+                ((time - last_vsync_end) < (FRAME_VISIBLE_AREA + FRAME_BACK_PORCH))
+            )
+        else:
+            visible_area = False
 
-            assert ((dut.uo_out.value & RGB_MASK) == 0) or not visible_area
+        assert ((dut.uo_out.value & RGB_MASK) == 0) or visible_area
 
-            if visible_area:
-                if last_vsync_start != None:
-                    r_value = int(str(dut.uo_out.value[0]) + str(dut.uo_out.value[4]), 2)
-                    g_value = int(str(dut.uo_out.value[1]) + str(dut.uo_out.value[5]), 2)
-                    b_value = int(str(dut.uo_out.value[2]) + str(dut.uo_out.value[6]), 2)
-                    f.write("{} {} {}    ".format(r_value, g_value, b_value))
-                    image.append((r_value, g_value, b_value))
+        if frame_nr != 0 and visible_area:
+            if last_vsync_start != None:
+                r_value = int(dut.red.value)
+                g_value = int(dut.green.value)
+                b_value = int(dut.blue.value)
+                f.write("{} {} {}    ".format(r_value, g_value, b_value))
+                image.append((r_value, g_value, b_value))
 
-                pixel_nr += 1
+        if visible_area:
+            pixel_nr += 1
 
-            old_vsync = int(dut.vsync.value)
-            old_hsync = int(dut.hsync.value)
+        # Handel the framebuffer
+        if dut.reset_read_ptr.value == 1 and old_reset_read_ptr == 0:
+            read_ptr_h = 0
+            read_ptr_v = 0
+            dut.log.info("Reset read pointer")
 
-            await ClockCycles(dut.clk, 1)
+        if dut.reset_write_ptr.value == 1 and old_reset_write_ptr == 0:
+            write_ptr = 0
+            dut.log.info("Reset write pointer")
+
+        if dut.write.value == 1 and old_write == 0:
+            framebuffer[write_ptr] = int(dut.write_data.value)
+            write_ptr += 1
+
+
+        if dut.read.value == 1 and old_read == 0:
+            read_ptr_h += 1
+
+            if read_ptr_h == WIDTH:
+                read_ptr_h = 0
+                read_ptr_v += 1
+
+        assert read_ptr_h < WIDTH
+        assert read_ptr_v // 2 <= HEIGHT
+
+        dut.ui_in.value = shift_framebuffer[0] * 16
+        shift_framebuffer = [framebuffer[read_ptr_h + (read_ptr_v // 2) * WIDTH], shift_framebuffer[1]]
+
+        old_vsync           = int(dut.vsync.value)
+        old_hsync           = int(dut.hsync.value)
+
+        old_write           = int(dut.write.value)
+        old_read            = int(dut.read.value)
+        old_reset_write_ptr = int(dut.reset_write_ptr.value)
+        old_reset_read_ptr  = int(dut.reset_read_ptr.value)
+
+        await ClockCycles(dut.clk, 1)
