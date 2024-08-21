dc6047ef8e1c7c17e8f89efe8964d23cf33a0ff9
Gerrit Fabian Grutzeck <ggrutzeck@mpifr-bonn.mpg.de>
2024-08-21 15:38:04 +0200

    Added dumping of all data in gl simulation

--- test/tb.v
+++ test/tb.v
@@ -9,7 +9,7 @@ module tb ();
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
-    $dumpvars(1, tb);
+    $dumpvars(2, tb);
     #1;
   end
 
